817390aaa723a67bda6e225fd1b2029cd63eb2a5
mohamedamgad <mishomishmish2@gmail.com>
2025-05-08 10:01:16 +0300

    create product entity and category entity and relation between them

--- services/product/src/main/java/com/ecommerce/entity/Category.java
+++ services/product/src/main/java/com/ecommerce/entity/Category.java
@@ -1,4 +1,22 @@
 package com.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
